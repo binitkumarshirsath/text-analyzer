24ca20455b187117a420f08e8361aed4a8d5d488
Binit <binitkumar0911@gmail.com>
2024-01-17 23:32:04 +0530

    added dummy data

--- src/components/ProcessWord.tsx
+++ src/components/ProcessWord.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import WordTable from "./WordTable";
 import { calculateWordsAndCharacters } from "../utils/helper";
+import { data } from "../utils/data";
 
 const ProcessWord = () => {
   const [text, setText] = useState("");
@@ -24,22 +25,22 @@ const ProcessWord = () => {
         </button>
       </div>
       <WordTable words={words} characters={characters} />
+      <div className="w-full p-3 flex flex-col border border-[#D2D6DB] mt-4">
+        {data.map((row, index) => {
+          return (
+            <div key={index} className="flex gap-4 w-full justify-start">
+              <div className="font-sans font-medium min-w-[100px] w-1/12 text-base text-[#475467]">
+                {row.name}
+              </div>
+              <div className="text-[#111927] font-sans font-medium text-base">
+                {row.value}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
 
 export default ProcessWord;
-
-{
-  /* <input
-          type="text"
-          autoFocus
-          className="h-10 mb-4 px-3 w-10/12 py-2 rounded-sm focus:none outline-none"
-          placeholder="Start typing here..."
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <button className="bg-[#731EE2] text-[#FFFFFF] px-3 py-2 font-sans font-normal rounded-sm  text-sm">
-          Process Word
-        </button> */
-}
---
+++ src/utils/data.ts
@@ -0,0 +1,19 @@
+export const data = [
+  {
+    name: "Definition",
+    value: `in, relating to, or characteristic of a town or city.
+        "the urban population"`,
+  },
+  {
+    name: "Parts of speech",
+    value: `noun`,
+  },
+  {
+    name: "Synonyms:",
+    value: `Town`,
+  },
+  {
+    name: "Antonyms",
+    value: `Rural`,
+  },
+];
